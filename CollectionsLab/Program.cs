b8b486a46ca8661e513e735c90317aa4a9254d69
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-08-12 12:19:55 -0400

    Add collections lab

---
+++ CollectionsLab/Program.cs
@@ -0,0 +1,30 @@
+﻿List<string> subdomains = new List<string>();
+
+subdomains.Add("git");
+subdomains.Add("me");
+subdomains.Add("info");
+
+Console.WriteLine("Available Subdomains");
+
+foreach (string subdomain in subdomains)
+{
+    Console.WriteLine(subdomain);
+}
+
+Dictionary<string, string> destinations = new Dictionary<string,string>();
+
+destinations.Add("git","https://github.com");
+destinations.Add("me","https://pablomendoza.site");
+destinations.Add("info","https://example.com");
+
+string requestedSubdomain = "youtube";
+
+if (destinations.TryGetValue(requestedSubdomain, out string? destination))
+{
+    Console.WriteLine($"Redirecting {requestedSubdomain} to:");
+    Console.WriteLine(destination);
+} 
+else
+{
+    Console.WriteLine("Subdomain not found");
+}
