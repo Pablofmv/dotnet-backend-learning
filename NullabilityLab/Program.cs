8513e6fa9b5ec6ca3e55e68a56310bff80718d62
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-08-13 15:48:13 -0400

    Nullability

---
+++ NullabilityLab/Program.cs
@@ -0,0 +1,20 @@
+﻿string subdomain = "git";
+string destinationUrl = "https://github.com";
+
+string? referrer = "https://google.com";
+
+string source = referrer ?? "Direct";
+
+Console.WriteLine($"Subdomain: {subdomain}");
+Console.WriteLine($"Destination: {destinationUrl}");
+Console.WriteLine($"Source: {source}");
+
+int? ReferrerLength = referrer?.Length;
+
+if (ReferrerLength != null) {
+    Console.WriteLine($"Referrer Length: {ReferrerLength}");
+} 
+else 
+{
+    Console.WriteLine($"No referrer lenght available");
+}
