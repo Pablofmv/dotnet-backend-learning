156f51f07628272d0047ad5d0d2a3c3f6334815f
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-08-17 15:03:56 -0400

    Completed Async

---
+++ AsyncLab/Program.cs
@@ -0,0 +1,42 @@
+﻿var links = new Dictionary<string, string>{
+    {"git","https://github.com"},
+    {"me","https://example.com"}
+};
+
+
+try {
+    string? destination = await FindDestinationAsync("",links);
+
+    if (destination is null)
+    {
+        Console.WriteLine($"404 Not Found");
+        return;
+    }
+
+    Console.WriteLine($"302 Redirect -> {destination}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"400 Bad Request -> {ex.Message}");
+}
+
+
+
+static async Task<string> FindDestinationAsync(
+    string subdomain,
+    Dictionary <string, string> links)
+{
+    if (string.IsNullOrWhiteSpace(subdomain))
+    {
+        throw new ArgumentException("subdomain cannot be empty");
+    }
+
+    await Task.Delay(500);
+
+    if (links.TryGetValue(subdomain, out string? destination))
+    {
+        return destination;
+    }
+
+    return null;
+}
