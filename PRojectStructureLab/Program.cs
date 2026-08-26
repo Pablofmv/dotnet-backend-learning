535970275c416dc1e4edf54896c208d7ebf43a2c
Pablo Mendoza <pablo_fmv@hotmail.com>
2026-08-26 12:49:51 -0400

    Completed Routing

--- PRojectStructureLab/Program.cs
+++ PRojectStructureLab/Program.cs
@@ -1,12 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// REGISTER
 
 builder.Services.AddControllers();
 
+
+// BUILD
 var app = builder.Build();
 
+//CONTROLLER ROUTES
 app.MapControllers();
+
+// FIXED ROUTE
 app.MapGet("/",() => "URL Shortener API is running");
+
+
+//ANOTHER FIXED ROUTE
+app.MapGet("/analytics",() => "URL Shortner Analytics");
+
+//DYNAMIC ROUTE
+app.MapGet("/links/{subdomain}",(string subdomain) => $"Looking up destination for : {subdomain}");
+
+//SAME PATH DIFFERENT HTTP METHOD
+app.MapPost("/links",() => "Creating a new short link");
 
 app.Run();
